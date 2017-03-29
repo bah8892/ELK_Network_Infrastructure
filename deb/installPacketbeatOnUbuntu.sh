5b1ca3a759a3f6f64b288e580e87b419a6f44ee3
Ben Hoffman <bah8892@g.rit.edu>
2017-03-29 16:04:04 -0400

    Started adding deb versions

---
+++ deb/installPacketbeatOnUbuntu.sh
@@ -0,0 +1,5 @@
+# This will Install packetbeat on Ubuntu
+
+sudo apt-get install libpcap0.8
+wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.3.0-amd64.deb
+sudo dpkg -i packetbeat-5.3.0-amd64.deb
