fbb3c531e11bb597939adba260808737ad5f177c
Ben Hoffman <bah8892@g.rit.edu>
2017-04-25 16:39:06 -0400

    Added the snort install script

--- rpm/installSnort.sh
+++ rpm/installSnort.sh
@@ -1,63 +1,20 @@
-#!/bin/bash
-
-set -x
-set -e
-
-if [ $# -ne 2 ]
-    then
-        echo "Wrong number of arguments supplied."
-        echo "Usage: $0 <server_url> <deploy_key>."
-        exit 1
-fi
-
-# Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-# Check if OS is CentOS
-if [ -f /etc/redhat-release ]; then
+############ Installing Snort on Centos 6 #####################
   echo "[`date`] ========= Installing updates ========="
   yum update -y && yum upgrade -y
+  yum install git vim curl -y
 
   # NTP Time Sync
   yum install ntp ntpdate ntp-doc -y
   systemctl enable ntpd
   systemctl start ntpd
   ntpdate pool.ntp.org || true
-else
-  echo "Please use CentOS to run this software :)"
-fi
 
-# Change hostname
-if [ $(hostname) == "localhost.localdomain" ]; then
-  hostnamectl set-hostname ip-$(hostname -I)
-fi
-
-# Get variables
-server_url=$1
-deploy_key=$2
-hostName=$(hostname)
-
-
-# Add new sensor to management nide
-result=$(curl -X POST http://$1:5000/newsensor/`hostname`/$2)
-
-if [ $result == "Honeypot not regisitered bad data*"]; then
-  echo $result
-  exit 1
-else
-  echo "Honeypot is registered with the following sensorID: $result"
-fi
-
-
-# Update system
-yum update -y && yum upgrade -y
 yum install git vim curl -y
 yum install epel-release -y
 
+
 ################################## Install/Setup Snort ##################################
+
 snortInterface=$(ip a | grep '2:' | grep 'en' | awk '{print $2}' |rev | cut -c 2- | rev)
 yum install -y gcc flex bison zlib libpcap pcre libdnet libdnet-devel tcpdump
 yum install -y https://www.snort.org/downloads/snort/daq-2.0.6-1.centos7.x86_64.rpm
@@ -66,7 +23,6 @@ ldconfig
 
 # Create directory
 sudo mkdir /usr/local/lib/snort_dynamicrules
-
 
 # Create files for white and black list rules and change permissions
 touch /etc/snort/rules/white_list.rules /etc/snort/rules/black_list.rules /etc/snort/rules/local.rules
@@ -129,6 +85,7 @@ ExecStart=/usr/bin/idstools-u2json  --snort-conf /etc/snort/snort.conf --directo
 WantedBy=multi-user.target
 EOF
 
+
 # Create Snort SystemD service
 cat > /etc/systemd/system/snort.service << EOF
 [Unit]
@@ -143,6 +100,7 @@ ExecStart=/usr/sbin/snort -q -u snort -g snort -c /etc/snort/snort.conf -i $snor
 WantedBy=multi-user.target
 EOF
 
+
 # Start and enable snort on boot
 systemctl enable snort
 systemctl start snort
@@ -151,10 +109,6 @@ systemctl enable snortLogging
 systemctl start snortLogging
 
 
-################################## Install/Setup Filebeat ##################################
-# If filebeat exists and is reporting to logstash just add new paths
-if rpm -qa | grep -qw filebeat; then
-
 # Just add config file
 cat > /etc/filebeat/conf.d/snort.yml << EOF
 filebeat.prospectors:
@@ -167,48 +121,7 @@ filebeat.prospectors:
   document_type: snort
 EOF
 
-else
-# Install filebeat
-sudo rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
-cat > /etc/yum.repos.d/elastic.repo << EOF
-[elastic-5.x]
-name=Elastic repository for 5.x packages
-baseurl=https://artifacts.elastic.co/packages/5.x/yum
-gpgcheck=1
-gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-enabled=1
-autorefresh=1
-type=rpm-md
-EOF
-
-sudo yum install filebeat -y
-systemctl enable filebeat
-systemctl start filebeat
-
-# Create config directory for filebeat
-mkdir /etc/filebeat/conf.d/
-cp /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.bak
-cat > /etc/filebeat/filebeat.yml << EOF
-filebeat:
-  registry_file: /var/lib/filebeat/registry
-  config_dir: /etc/filebeat/conf.d
-
-output.logstash:
-  hosts: ["$1:5044"]
-EOF
-
-# Just add config file
-cat > /etc/filebeat/conf.d/snort.yml << EOF
-filebeat.prospectors:
-- input_type: log
-  paths:
-    - /var/log/snort/*.json
-  fields:
-    sensorID: $result
-    sensorType: networksensor
-  document_type: snort
-EOF
-
-
-fi
 systemctl restart filebeat
+
+systemctl status snort
+systemctl status snortLogging
